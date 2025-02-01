d2a5fcfb41107b427e38f83421f8ca48d518e8dd
zadvajr <danielzadva@gmail.com>
2025-02-02 00:48:30 +0100

    02-02-25: drop database statement

---
+++ drop_db_testDB.sql
@@ -0,0 +1,16 @@
+-- MySQL DROP DATABASE Statement
+-- The MySQL DROP DATABASE Statement
+-- The DROP DATABASE statement is used to drop an existing SQL database.
+
+-- Syntax
+-- DROP DATABASE databasename;
+
+-- Note: Be careful before dropping a database.
+-- Deleting a database will result in loss of complete information stored in the database!
+
+-- DROP DATABASE Example
+-- The following SQL statement drops the existing database "testDB":
+
+-- Example
+
+DROP DATABASE testDB;
