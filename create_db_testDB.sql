5f3e435ddb3c9f8c5ec87b17a9765c2d0d937905
zadvajr <danielzadva@gmail.com>
2025-02-02 00:30:04 +0100

    02-02-25: create_db_testDB

---
+++ create_db_testDB.sql
@@ -0,0 +1,10 @@
+-- MySQL CREATE DATABASE Statement
+-- The MySQL CREATE DATABASE Statement
+-- The CREATE DATABASE statement is used to create a new SQL database.
+
+-- Syntax
+-- CREATE DATABASE databasename;
+
+-- 
+
+CREATE DATABASE testDB;
