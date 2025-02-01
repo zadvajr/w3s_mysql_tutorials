0c2464b940131c1b97f3c3e4bb4d39395d1267d8
zadvajr <danielzadva@gmail.com>
2025-02-02 00:40:31 +0100

    02-02-25: show databases statement

--- create_db_testDB.sql
+++ create_db_testDB.sql
@@ -8,3 +8,7 @@
 -- 
 
 CREATE DATABASE testDB;
+
+-- Tip: Make sure you have admin privilege before creating any database.
+-- Once a database is created, you can check it in the list of databases with the following SQL command:
+-- SHOW DATABASES;
